7becc6c657b9f34814bb604aa60a243ea1f3f783
Chairun Nisaq <145679533+broomrun@users.noreply.github.com>
2025-12-06 17:29:57 +0700

    add release configuration and update splash screen - implement signing configuration for release builds, enhance launch background with new images, and set up GitHub Actions for automated APK builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,8 @@
+import java.util.Properties
+import java.io.File
+import java.io.FileInputStream
+
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -9,8 +14,17 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+    // Load keystore properties
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystoreProperties = Properties()
+    if (keystorePropertiesFile.exists()) {
+        FileInputStream(keystorePropertiesFile).use {
+            keystoreProperties.load(it)
+        }
+    }
+
 android {
-    namespace = "com.example.animeverse_2"
+    namespace = "com.example.animeverse"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,9 +37,19 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as? String
+            keyPassword = keystoreProperties["keyPassword"] as? String
+            // ensure storeFile is converted to String before calling file(...)
+            storeFile = (keystoreProperties["storeFile"] as? String)?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as? String
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.animeverse_2"
+        applicationId = "com.example.animeverse"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -38,7 +62,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
